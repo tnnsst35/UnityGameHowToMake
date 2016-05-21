1df3a0fa9a74e10d781d4f9dc8eef520e5a82100
tnnsst35 <s.taninishi@gmail.com>
2016-05-21 22:43:20 +0900

    Tutorial

---
+++ Tutorial/Assets/Scripts/Launcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Instantiate(this.ballPrefab);
+		}
+	}
+}
---
+++ Tutorial/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	protected float mJumpSpeed = 12.0f;
+	public    bool  isLanding = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isLanding) {
+			if (Input.GetMouseButtonDown(0)) {
+				this.isLanding = false;
+				this.GetComponent<Rigidbody>().velocity = Vector3.up * this.mJumpSpeed;
+			}
+		}
+	}
+	
+	// 他のゲームオブジェクトと当たったら
+	void OnCollisionEnter(Collision collistion)
+	{
+		if (collistion.gameObject.tag == "Floor") {
+			this.isLanding = true;
+		}
+	}
+}
